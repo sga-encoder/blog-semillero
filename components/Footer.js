0defce7158b6c6f59744368ba55605c7293aa3eb
sga-encoder <sga4111@gmail.com>
2022-11-06 00:22:07 -0500

    fix        }

--- components/Footer.js
+++ components/Footer.js
@@ -3,7 +3,7 @@ import style from '../styles/Footer.module.css'
 
 const Footer = () => {
   return (
-    <div className={style.Footer}> 
+    <div className={style.Footer}>
       Creado por <span> Sebastian Garzon Arias </span> - IG: <a href="https://www.instagram.com/sga_28_/" target="_blank" rel="noopener noreferrer"> @sga_28_</a>
     </div>
   )
