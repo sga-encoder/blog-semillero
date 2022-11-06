29c59eb29cca11cfac2b4c84b6702091d7066ead
sga-encoder <sga4111@gmail.com>
2022-11-05 23:17:06 -0500

    solution _app

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import '../styles/globals.css'
+import React from 'react'
 
-function MyApp({ Component, pageProps }) {
+function MyApp ({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
 
